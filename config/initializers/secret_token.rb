a8854f51ddcbbe2460c32cbe3c6275c0031c329f
don <don@curulubuntu.(none)>
2012-04-14 08:00:00 -0400

    rails created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Widgetgarden::Application.config.secret_token = 'f77ceedd2aa96fd87e0281ba1085ab13dd5ed336acf6b4f1bb226fc9a456bc1f8d10b177bca4c08cea7e2aab8648f80598f621e10c2ce1c5379e24a6f4b54818'
